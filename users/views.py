b480c97b878030c5b74816a5a99a59b494c96435
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-13 15:30:57 +0300

    logout functionality

--- users/views.py
+++ users/views.py
@@ -10,9 +10,9 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
+            messages.success(request, f'Your account has been created! You have access to log in')
             
-            return redirect('home')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
         
