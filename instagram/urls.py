3bf23f6e2f6a402a48cdbee6b2e347e45c4302e8
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-13 14:54:08 +0300

    user registration system

--- instagram/urls.py
+++ instagram/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 # from django.conf.urls import path, include
 from django.urls import path, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view, name='login'),
+    path('logout/', auth_views.LogoutView, name='logout'),
     path('',include('photos.urls')),
+
+
 ]
