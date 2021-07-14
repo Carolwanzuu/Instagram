73e7f730fd5ab0c32638ed6b2b55cf98950a5b94
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-14 13:46:13 +0300

    added a comments model for photos

--- photos/models.py
+++ photos/models.py
@@ -13,3 +13,8 @@ class Post(models.Model):
 
      def __str__(self):
          return self.name
+
+class Comments(models.Model):
+  comment = models.CharField(max_length=200, null=True, blank=True)
+  pic = models.ForeignKey(Post, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
